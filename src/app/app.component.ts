2da4d86255625f85ed817c7a1504af9232db9039
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-03-07 18:19:25 +0200

    sending a post request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  link = 'https://ng-guide-d3833-default-rtdb.firebaseio.com/'
   loadedPosts = [];
+  subscription: Subscription;
 
   constructor(private http: HttpClient) {}
 
@@ -16,6 +19,12 @@ export class AppComponent implements OnInit {
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     console.log(postData);
+    // this.http.post('https://ng-guide-d3833-default-rtdb.firebaseio.com/')
+    // this.http.post('https://ng-guide-d3833-default-rtdb.firebaseio.com/posts.json', postData)
+    this.subscription =  this.http.post(`${this.link}posts.json`, postData)
+    .subscribe(responseData => {
+      console.log(responseData); //{name: "-Mx_8nE_2GjdHUjuuVb5"}
+    });
   }
 
   onFetchPosts() {
@@ -24,5 +33,9 @@ export class AppComponent implements OnInit {
 
   onClearPosts() {
     // Send Http request
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 }
