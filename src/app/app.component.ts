46eaacc742a6c3cf19d6ca1f595d717238343f82
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-03-08 13:21:48 +0200

    RxJS Operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subscription } from 'rxjs';
-
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -43,8 +43,24 @@ export class AppComponent implements OnInit, OnDestroy {
 
   private fetchPosts() {
     this.subscription =  this.http.get(`${this.link}posts.json`)
+    .pipe(map(responseData => {
+      console.log(responseData) // 1 <--- all posts are here : {-Mx_6z74J0tAKhr-frO5: {…}, -Mx_7NFZQC8faH844WXZ: {…}, -Mx_8Ls-DUr85ZHa-mz9: {…}, -Mx_8nE_2GjdHUjuuVb5: {…}, -Mx_9EKyHAKFWZwkR9xo: {…}}
+       const postsArray = [];
+       for(const key in responseData) {
+
+        if(responseData.hasOwnProperty(key)){
+         // console.log(responseData[key]) // every post separately
+         postsArray.push({...responseData[key], id: key});
+        }
+        
+       }
+       console.log(postsArray) // all posts are here after all, as objects in one array
+       return postsArray;
+    }))
     .subscribe(posts=>{
-      console.log(posts);  //<--- all posts are here : {-Mx_6z74J0tAKhr-frO5: {…}, -Mx_7NFZQC8faH844WXZ: {…}, -Mx_8Ls-DUr85ZHa-mz9: {…}, -Mx_8nE_2GjdHUjuuVb5: {…}, -Mx_9EKyHAKFWZwkR9xo: {…}}
+      //...
+      console.log(posts);  //2<--- all posts are here : {-Mx_6z74J0tAKhr-frO5: {…}, -Mx_7NFZQC8faH844WXZ: {…}, -Mx_8Ls-DUr85ZHa-mz9: {…}, -Mx_8nE_2GjdHUjuuVb5: {…}, -Mx_9EKyHAKFWZwkR9xo: {…}}
+      // now it is undefined , after postsArray.push({...responseData[key]});
     });
   }
 }
