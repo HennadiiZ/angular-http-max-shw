727499634087c038fbc43127f661d4f05a3b93b0
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-03-09 13:24:58 +0200

    response interceptors

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,15 +1,22 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { tap } from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any> , next: HttpHandler){
-        console.log("Request is on its way",  req);
-        // const midifiedRequest = req.clone({url: 'some-new-url'});
-        // const midifiedRequest = req.clone({headers: req.headers.append});
-        console.log('req.url',  req.url)
+        //console.log("Request is on its way",  req);
+
+        //console.log('req.url',  req.url)
         const midifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')});
-        console.log('midifiedRequest', midifiedRequest)
-        //const midifiedRequest = req.clone({params: req.params});
-        return next.handle(midifiedRequest);
-        // return next.handle(req);
+        //console.log('midifiedRequest', midifiedRequest)
+
+        // return next.handle(midifiedRequest); // it gives an Observable
+        return next.handle(midifiedRequest).pipe(tap(event =>{
+              
+            console.log(event);
+            if(event.type === HttpEventType.Response){
+                console.log('Response is here:', event.body);
+            }
+        }))
+
     }
 }
